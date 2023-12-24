cfc18d2f32a0a4ebc8653dc0b8fd2f4fa1ff95fe
Rishabh Singh <52191143+rishabh0085@users.noreply.github.com>
2023-12-24 21:23:09 +0530

    Update Main.java

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,19 +1,4 @@
 package com.driver;
-
-public class RWOnly {
-    // Task 2: Create a class named RWOnly in RWOnly.java with a private member variable name
-    private String name;
-
-    // Task 5: Implement getter and setter methods
-    public String getName() {
-        return name;
-    }
-  
-    public void setName(String name) {
-        this.name = name;
-    }
-}
-
 public class Main {
     public static void main(String[] args) {
         // Task 3: Create an object of class RWOnly in this file
