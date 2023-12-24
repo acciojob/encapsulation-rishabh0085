43e942bdf0eeadd88aadfd080ba49dab20df401e
Rishabh Singh <52191143+rishabh0085@users.noreply.github.com>
2023-12-24 21:22:49 +0530

    Update RWOnly.java

--- src/main/java/com/driver/RWOnly.java
+++ src/main/java/com/driver/RWOnly.java
@@ -1,5 +1,16 @@
 package com.driver;
 
 public class RWOnly {
-    
+    // Task 2: Create a class named RWOnly in RWOnly.java with a private member variable name
+    private String name;
+
+    // Task 5: Implement getter and setter methods
+    public String getName() {
+        return name;
+    }
+  
+    public void setName(String name) {
+        this.name = name;
+    }
 }
+
