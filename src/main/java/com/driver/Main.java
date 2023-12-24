94c8269d6731353a977b18a206ad9bc8d8534325
Rishabh Singh <52191143+rishabh0085@users.noreply.github.com>
2023-12-24 21:16:57 +0530

    Update Main.java

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,24 @@
 package com.driver;
 
-public class Main {
+public class RWOnly {
+    // Task 2: Create a class named RWOnly in RWOnly.java with a private member variable name
+    private String name;
+
+    // Task 5: Implement getter and setter methods
+    public String getName() {
+        return name;
+    }
   
+    public void setName(String name) {
+        this.name = name;
+    }
+}
+
+public class Main {
+    public static void main(String[] args) {
+        // Task 3: Create an object of class RWOnly in this file
+        RWOnly obj = new RWOnly();            
+        obj.setName("John");  // Task 4: Try setting a value to name by directly accessing it using obj
+        System.out.println("Name: " + obj.getName()); // Task 6: Set a value to name using setter function and access it using getter function
+    }
 }
